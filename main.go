6d27d8b87c196f3981b055ccd86f36c6913d5709
Charlie MacFarlane Brodie <mail@charliebrodie.com>
2020-04-27 12:15:51 +0100

    Started working on pulling down the hugo themes.

--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"strings"
+	"log"
     "fmt"
-)
+    "os"
+    "github.com/go-git/go-git"
+    "io/ioutil"
+)	
+
+const themes string = "https://github.com/gohugoio/hugoThemes"
+const themesDirectory string = "./themes"
 
 func main() {
+	getHugoTheme()
     fmt.Println("Welcome to the hugo theme switcher.")
+}
+
+
+func getHugoTheme() {
+	//pull latest version of hugo themes repo
+	if _, err := os.Stat(themesDirectory); os.IsNotExist(err) {
+
+		_, err := git.PlainClone(themesDirectory, false, &git.CloneOptions{
+			URL:      themes,
+			Progress: os.Stdout,
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// The themes in this repository are actually stored as submodules on the main repo,
+	// therefore the only file we actually need in order to access them is .gitmodules as
+	// that contains a list of all available themes 
+    modulesFile, err := ioutil.ReadFile(themesDirectory + "/.gitmodules")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Split the file out into its individual lines
+	lines := strings.Split(string(modulesFile), "\n")
+	fmt.Println(lines[0])
+	//add all themes to channel
+
+	// select a random repo
+
+	// download the theme
 }
