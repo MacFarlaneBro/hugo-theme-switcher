84675044db6d455f2571dc290a9e3fb889122b4a
Charlie MacFarlane Brodie <mail@charliebrodie.com>
2020-04-27 16:10:28 +0100

    Tidied up file and progressed.

--- main.go
+++ main.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"strings"
+	"fmt"
+	"io/ioutil"
 	"log"
-    "fmt"
-    "os"
-    "github.com/go-git/go-git"
-    "io/ioutil"
-)	
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-git/go-git"
+)
 
 const themes string = "https://github.com/gohugoio/hugoThemes"
 const themesDirectory string = "./themes"
 
 func main() {
 	getHugoTheme()
-    fmt.Println("Welcome to the hugo theme switcher.")
+	fmt.Println("Welcome to the hugo theme switcherdafd.")
 }
-
 
 func getHugoTheme() {
 	//pull latest version of hugo themes repo
 	if _, err := os.Stat(themesDirectory); os.IsNotExist(err) {
 
 		_, err := git.PlainClone(themesDirectory, false, &git.CloneOptions{
-			URL:      themes,
-			Progress: os.Stdout,
+			URL: themes,
 		})
 
 		if err != nil {
@@ -34,8 +35,8 @@ func getHugoTheme() {
 
 	// The themes in this repository are actually stored as submodules on the main repo,
 	// therefore the only file we actually need in order to access them is .gitmodules as
-	// that contains a list of all available themes 
-    modulesFile, err := ioutil.ReadFile(themesDirectory + "/.gitmodules")
+	// that contains a list of all available themes
+	modulesFile, err := ioutil.ReadFile(themesDirectory + "/.gitmodules")
 
 	if err != nil {
 		log.Fatal(err)
@@ -43,10 +44,28 @@ func getHugoTheme() {
 
 	// Split the file out into its individual lines
 	lines := strings.Split(string(modulesFile), "\n")
-	fmt.Println(lines[0])
-	//add all themes to channel
+
+	// Remove all non-url lines
+	urlFilter := func(s string) bool { return strings.HasPrefix(s, "url") }
+
+	//add all themes to a slice
+	urls := filter(lines, urlFilter)
 
 	// select a random repo
+	rand.Seed(time.Now().Unix())
+	fmt.Println(urls[rand.Intn(len(urls))])
 
 	// download the theme
+}
+
+//Filter function lifted from here: https://stackoverflow.com/questions/37562873/most-idiomatic-way-to-select-elements-from-an-array-in-golang#answer-37563128
+// Given an array of strings return an array of only those strings which are true according to the supplied 'test' function
+func filter(lines []string, test func(string) bool) (ret []string) {
+	for _, s := range lines {
+		url := strings.TrimSpace(s)
+		if test(url) {
+			ret = append(ret, url[6:])
+		}
+	}
+	return
 }
